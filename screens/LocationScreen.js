a4ebb60a6c2ab17bd95ee5230845912bfd118239
Santosh Joseph <sjoseph@groupon.com>
2019-08-31 20:02:31 -0700

    refactor

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -8,18 +8,12 @@ import { ONWATER, MAPKEY } from 'react-native-dotenv'
 
 export default class LocationScreen extends Component {
   state = {
-    location: null,
-    trueLocation: null,
-    heading: null,
-    errorMessage: null,
-    loading: true,
-    landWater: null,
-    addresses: null,
-    magLocation: null,
-    text: '1',
+    loading: false,
+    numStr: '1',
     locationStr: '',
+    initialLoad: true,
+    overWater: false,
   };
-  distance = 1;
 
   componentWillMount() {
     if (Platform.OS === 'android' && !Constants.isDevice) {
@@ -27,14 +21,14 @@ export default class LocationScreen extends Component {
         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
       });
     } else {
-      this._getLocationAsync();
+      //this._getLocationAsync();
       // Location.watchHeadingAsync((obj) => {
       //   console.log('WATCH:', obj);
       // })
     }
   }
 
-  _getLocationAsync2 = async () => {
+  _getLocationAsync2 = async (distance) => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== 'granted') {
       this.setState({
@@ -42,69 +36,40 @@ export default class LocationScreen extends Component {
       });
       return;
     }
+    this.setState({ initialLoad: false, loading: true });
     let location = await Location.getCurrentPositionAsync({});
     let heading = await Location.getHeadingAsync();
     const current = new LatLon(location.coords.latitude, location.coords.longitude);
-    const trueLocation = current.destinationPoint(this.distance * 1000, heading.trueHeading);
-    const magLocation = current.destinationPoint(this.distance * 1000, heading.magHeading);
+    const trueLocation = current.destinationPoint(distance * 1000, heading.trueHeading);
+    const magLocation = current.destinationPoint(distance * 1000, heading.magHeading);
     console.log('TRUE:', trueLocation);
     console.log('MAG:', magLocation);
+    const landWater = await (await fetch(`https://api.onwater.io/api/v1/results/${trueLocation._lat},${trueLocation._lon}?access_token=${ONWATER}`)).json();
+    if (landWater.water) {
+      this.setState({ overWater: true, loading: false, locationStr: '', errorMessage: 'Over water' });
+      return;
+    }
+    const googleResult = await (await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${landWater.lat},${landWater.lon}&key=${MAPKEY}`)).json();
+    if (googleResult.status === 'OK') {
+      const addresses = [];
+      for (let i = 0; i < googleResult.results.length; i++) {
+        console.log('address:', googleResult.results[i].formatted_address);
+        addresses.push(googleResult.results[i].formatted_address);
+      }
+      this.setState({ locationStr: addresses[0], loading: false, overWater: false })
+    } else {
+      this.setState({ errorMessage: 'Could not find location:' + googleResult.status })
+    }
   }
 
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied',
-      });
-      return;
-    }
-    this.setState({ loading: true });
-    let location = await Location.getCurrentPositionAsync({});
-    // console.log('LOCATION:', location);
-    let heading = await Location.getHeadingAsync();
-    console.log('HEADING:', heading);
-
-    const current = new LatLon(location.coords.latitude, location.coords.longitude);
-    const trueLocation = current.destinationPoint(this.distance * 1000, heading.trueHeading);
-    const magLocation = current.destinationPoint(this.distance * 1000, heading.magHeading);
-    console.log('TRUE:', trueLocation);
-    console.log('MAG:', magLocation)
-    const landWater = await (await fetch(`https://api.onwater.io/api/v1/results/${trueLocation._lat},${trueLocation._lon}?access_token=${ONWATER}`)).json();
-    console.log('WATER:', landWater.water);
-
-    const addresses = [];
-    if (!landWater.water) {
-      const googleResult = await (await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${landWater.lat},${landWater.lon}&key=${MAPKEY}`)).json();
-      if (googleResult.status === 'OK') {
-        console.log('RESULTS:', googleResult.results.length);
-        for (let i = 0; i < googleResult.results.length; i++) {
-          console.log('address:', googleResult.results[i].formatted_address);
-          addresses.push(googleResult.results[i].formatted_address);
-        }
-        this.setState({locationStr: googleResult.results[0]})
-        console.log('GOOGLE:', googleResult.plus_code.compound_code);
-      } else {
-        console.log('google result:', googleResult.status);
-        this.setState({errorMessage: `Could not get result from Google: ${googleResult.status}`});
-        return;
-      }
-    } else {
-      this.setState({errorMessage: 'This location is in water :('}) ;
-      return;
-    }
-    this.setState({ location, trueLocation, magLocation, heading, loading: false, landWater, addresses });
-  };
-
   onRefresh = async () => {
-    console.log('REFRESH:', this.distance);
-    this._getLocationAsync();
+    console.log('REFRESH:', this.state.numStr)
+    this._getLocationAsync2(parseInt(this.state.numStr));
   };
 
   onMileage = (distance) => {
     console.log('MILEAGE:', distance);
-    this.distance = distance;
-    this._getLocationAsync();
+    this._getLocationAsync2(distance);
   };
 
   render() {
@@ -116,29 +81,40 @@ export default class LocationScreen extends Component {
         text = this.state.locationStr;
       }
     } else {
-      text = 'Checking...';
+      if (this.state.initialLoad) {
+        text = '';
+      } else {
+        text = 'Checking...';
+      }
     }
 
     return (
       <ScrollView contentContainerStyle={styles.container}>
-       <View
+        <View
           style={styles.inputsView}>
-         <TextInput
-          style={{ height: 40 }}
-          keyboardType="number-pad"
-          placeholder="Distance in KM"
-          onChangeText={(text) => {
-            this.setState({ text });
-            this.distance = parseInt(text);
-          }}
-          value={this.state.text}
-         />
-          <TouchableOpacity onPress={this.onRefresh} style={styles.button}><Button title="Check"/></TouchableOpacity>
+          <TextInput
+            style={{ height: 40, width: 100, borderStyle: 'solid', borderWidth: 1 }}
+            keyboardType="number-pad"
+            placeholder="Distance in KM"
+            onChangeText={(numStr) => {
+              this.setState({ numStr });
+            }}
+            value={this.state.numStr}
+          />
+          <TouchableOpacity onPress={() => this.onRefresh()} style={styles.button}>
+            <Text style={styles.button}>Check</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.inputsView}>
-          <TouchableOpacity onPress={this.onMileage.bind(this, 10)} style={styles.button}><Button title="10 km" /></TouchableOpacity>
-          <TouchableOpacity onPress={this.onMileage.bind(this, 100)} style={styles.button}><Button title="100 km" /></TouchableOpacity>
-          <TouchableOpacity onPress={this.onMileage.bind(this, 1000)} style={styles.button}><Button title="1000 km" /></TouchableOpacity>
+          <TouchableOpacity onPress={this.onMileage.bind(this, 10)} style={styles.button}>
+            <Text style={styles.button}>10 km</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onMileage.bind(this, 100)} style={styles.button}>
+            <Text style={styles.button}>100 km</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onMileage.bind(this, 1000)} style={styles.button}>
+            <Text style={styles.button}>1000 km</Text>
+          </TouchableOpacity>
         </View>
         <View>
           <Text style={styles.paragraph}>{text}</Text>
@@ -150,9 +126,9 @@ export default class LocationScreen extends Component {
 
 const styles = StyleSheet.create({
   inputsView: {
-            flexDirection: 'row',
-            height: 100,
-            padding: 20,
+    flexDirection: 'row',
+    height: 100,
+    padding: 20,
   },
   container: {
     // flex: 1,
@@ -167,7 +143,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   button: {
-    backgroundColor: '#DDDDDD',
+    textTransform: 'uppercase',
+    backgroundColor: 'blue',
+    color: 'white',
     margin: 10,
   }
 });
